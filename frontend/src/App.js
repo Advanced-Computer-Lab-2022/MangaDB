a1a545cd0bd8a5180ab46585530c50596cfcf98e
mina0magdy <92601097+mina0magdy@users.noreply.github.com>
2022-10-27 18:25:47 +0200

    Added courseCardHover component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import CourseCard from "./components/Course/CourseCard";
 import NavBar from "./components/NavBar";
 import PrimaryButton from "./components/PrimaryButton";
-
+import CourseCardHover from "./components/CourseCardHover";
 import CountryManager from "./components/CountryManager";
 import HomePage from "./containers/HomePage";
+import Search from "./components/Search";
+import AddUserForm from "./components/AddUserForm";
 function App() {
   const test = {
     image: `https://i0.wp.com/blog.frontiersin.org/wp-content/uploads/2018/06/frontiers-in-ecology-evolution-ape-human-bonobo-muscles.jpg?resize=940%2C529&ssl=1`,
@@ -18,14 +20,8 @@ function App() {
     //text: "Home",
   };
   return (
-    // <CourseCard {...test}>
-
-    // </CourseCard>
     <div>
-      {/* <NavBar />
-      <HomePage /> */}
-      {/* <NavBar /> */}
-      <CourseCard {...test}/>
+      <AddUserForm />
     </div>
   );
 }

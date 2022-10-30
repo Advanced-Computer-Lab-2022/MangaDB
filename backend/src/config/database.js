d5cd07b6f7d4e859756ed6283c7ff844cf79ad49
Marwan Ashraf <Marwan_thelegend@hotmail.com>
2022-10-30 11:57:49 +0200

    adjusted course controller and return currency symbol and some performance related adjustments

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,10 +6,10 @@ const conn = process.env.DB_STRING;
 const connection = mongoose.createConnection(conn, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, (err) => { console.log(err); });
+}, (err) => { 
+    if(err) {
+    console.log(err);} });
 
-// Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
 
 
-// Expose the connection
 module.exports = connection;

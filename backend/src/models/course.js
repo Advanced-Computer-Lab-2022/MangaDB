3fb4e1f450ff906195255d3494913845dc9b8a1c
Marwan Ashraf <Marwan_thelegend@hotmail.com>
2022-12-16 15:47:01 +0200

    idk

--- backend/src/models/course.js
+++ backend/src/models/course.js
@@ -48,6 +48,9 @@ const courseSchema = new mongoose.Schema({
           type: mongoose.Schema.Types.ObjectId,
           ref: "User",
         },
+        userName: {
+          type: String,
+        },
         review: {
           type: String,
         },

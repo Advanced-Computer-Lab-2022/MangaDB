9abb0faea4d0a24c99872cb8cdf826a1d193688c
Marwan Ashraf <Marwan_thelegend@hotmail.com>
2022-12-16 17:51:21 +0200

    fixed bugs

--- backend/src/routes/course.js
+++ backend/src/routes/course.js
@@ -23,5 +23,24 @@ router.get("/mostrated", courseController.getMostRatedCourses);
 router.post("/addsubtitle/:id", courseController.addSubtitle);
 
 
+// router.get("/", courseController.getAllCourses);
+
+// router.get("/:id", courseController.getCourse);
+
+// router.patch("/:id",auth.validateToken,auth.authenticateRole(["INSTRUCTOR"]), courseController.updateCourse);
+
+// router.delete("/:id",auth.validateToken,auth.authenticateRole(["INSTRUCTOR"]), courseController.deleteCourse);
+
+// router.get("/rate/:id", courseController.getCourseRating);
+
+// router.post("/rate/:id",auth.validateToken,auth.authenticateRole(["TRAINEE","CORPORATE"]), courseController.rateCourse);
+
+// router.patch("/rate/:id",auth.validateToken,auth.authenticateRole(["TRAINEE","CORPORATE"]), courseController.editRating);
+
+// router.get("/mostviewed", courseController.getMostViewedCourses);
+
+// router.get("/mostrated", courseController.getMostRatedCourses);
+
+// router.post("/addsubtitle/:id", courseController.addSubtitle);
 
 module.exports = router;

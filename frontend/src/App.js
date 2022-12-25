4b425cc5744d2a1e3e393ad6c72469b053d9db14
MichelRaouf <68081786+MichelRaouf@users.noreply.github.com>
2022-12-25 19:16:13 +0200

    mycourses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import SignUp from "./pages/SignUp";
 import ResetPassword from "./pages/ResetPassword";
 import Footer from "./components/UI/Footer/Footer";
 import FAQS from "./pages/FAQS";
+import MyCourses from "./pages/MyCourses";
 function App() {
   return (
     // navbar should be here
@@ -55,6 +56,7 @@ function App() {
           element={<AddCoursePage></AddCoursePage>}
         ></Route>
         <Route path="FAQS" element={<FAQS></FAQS>}></Route>
+        <Route path ="myCourses" element={<MyCourses></MyCourses>}></Route>
       </Routes>
       <Footer></Footer>
     </Fragment>

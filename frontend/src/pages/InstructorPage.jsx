55c50e613397abec9ee9ad7296178f75795b318c
OmarMoataz19 <omarmoataz1942@gmail.com>
2022-12-27 16:35:47 +0200

    almost done with instructor page

--- frontend/src/pages/InstructorPage.jsx
+++ frontend/src/pages/InstructorPage.jsx
@@ -5,7 +5,6 @@ import SecondaryButton from "../components/UI/SecondaryButton";
 import AverageSummary from "../components/Profile/Reviews/AverageSummary";
 import ReviewItem from "../components/CourseDetailsComp/ReviewItem";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Pagination from "@mui/material/Pagination";
 import CourseCard from "../components/Course/CourseCard";
 
 import {
@@ -13,29 +12,7 @@ import {
   CalendarIcon,
   UsersIcon,
 } from "@heroicons/react/outline";
-
 import Rating from "@mui/material/Rating";
-const stats = [
-  {
-    id: 1,
-    name: " Number Of Students",
-    stat: "71,897",
-    icon: UsersIcon,
-  },
-  {
-    id: 2,
-    name: `Number Of Reviews `,
-    stat: `2000`,
-    icon: CalendarIcon,
-  },
-  {
-    id: 3,
-    name: `Number Of Courses`,
-    stat: "30",
-    icon: BookOpenIcon,
-  },
-];
-
 // pagination blue theme
 const theme = createTheme({
   status: {
@@ -52,37 +29,54 @@ const theme = createTheme({
     },
   },
 });
-const reviews = [
-  {
-    date: "2022-12-27T10:15:58.506+00:00",
-    userName: "Omar Moataz",
-    rating: 1,
-    review: "this is so shit ",
-  },
-  {
-    date: "2022-12-27T10:15:58.506+00:00",
-    userName: "Omar Moataz",
-    rating: 4,
-    review: "this is so shit ",
-  },
-  {
-    date: "2022-12-27T10:15:58.506+00:00",
-    userName: "Omar Moataz",
-    rating: 2,
-    review: "this is so shit ",
-  },
-];
+
 const InstructorPage = () => {
   const [receivedData, setReceivedData] = useState({});
   const [enteredReview, setEnteredReview] = useState("");
   const [enteredRating, setEnteredRating] = useState("");
-  const [page, setPage] = useState(1);
-  const [noOfPages, setNoOfPages] = useState(0);
+  const [statsConf, setStatsConf] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  //fetch the data at the start of the code ..
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/instructor/63a36fd41bd9f2e6163b0481")
+      .then((res) => {
+        setReceivedData(res.data);
+        setReviews(res.data.instructor.reviews);
+        setStatsConf([
+          res.data.students,
+          res.data.instructor.reviews.length,
+          res.data.instructor.courseDetails.length,
+        ]);
+        setLoading(false);
+      });
+  }, []);
 
-  //funtion to handle the pagination
-  const onChangePageHandler = (event, value) => {
-    setPage(value);
-  };
+  //stats for the performance thinggy
+  var stats;
+  if (statsConf.length !== 0) {
+    stats = [
+      {
+        id: 1,
+        name: " Number Of Students",
+        stat: receivedData.students,
+        icon: UsersIcon,
+      },
+      {
+        id: 2,
+        name: `Number Of Reviews `,
+        stat: receivedData.instructor.reviews.length,
+        icon: CalendarIcon,
+      },
+      {
+        id: 3,
+        name: `Number Of Courses`,
+        stat: receivedData.instructor.courseDetails.length,
+        icon: BookOpenIcon,
+      },
+    ];
+  }
 
   //submit the instructor review
   const onClickHandler = () => {
@@ -91,7 +85,14 @@ const InstructorPage = () => {
       rating: enteredRating,
       review: enteredReview,
     };
-    //axios submit review instructor review here
+    axios
+      .post(
+        "http://localhost:3000/instructor/rate/63a36fd41bd9f2e6163b0481",
+        reviewData
+      )
+      .then((res) => {
+        setReviews([...reviews, res.review]);
+      });
   };
 
   //listen on the rating change
@@ -103,6 +104,7 @@ const InstructorPage = () => {
     setEnteredReview(event.target.value);
   };
 
+  //handle the displayed Reviews
   var displayedReviews = [];
   if (reviews !== []) {
     displayedReviews = reviews.map((review) => {
@@ -146,106 +148,117 @@ const InstructorPage = () => {
   } else {
     displayedReviews = (
       <div className="mt-4 flex justify-center font-medium">
-        No Reviews Found.
+        No Reviews Found For This Instructor.
       </div>
+    );
+  }
+  //handle the displayed instructor courses
+  if (!loading) {
+    var displayedCourses = receivedData.instructor.courseDetails.map(
+      (course) => {
+        return (
+          <CourseCard
+            id={course.course._id}
+            duration={course.course.totalHours}
+            title={course.course.courseTitle}
+            instructorName={course.course.instructorName}
+            subject={course.course.subject}
+            level={course.courselevel}
+            description={course.course.courseDescription}
+            coursePrice={course.course.coursePrice}
+            discountedPrice={course.course.discountedPrice}
+            discount={course.course.discount}
+            rating={course.course.rating}
+            currencySymbol="$"
+          ></CourseCard>
+        );
+      }
     );
   }
 
   return (
     <Fragment>
       <NavBar></NavBar>
-      <div className="flex-col items-center justify-center">
-        <p className=" flex items-center justify-center  font-semibold text-gray-500">
-          INSTRUCTOR
-        </p>
-        <p className=" flex items-center justify-center font-bold text-4xl">
-          Omar Moataz
-        </p>
-        <p className="flex items-center justify-center text-gray-500">
-          About Me
-        </p>
-        <p className="flex items-center justify-center text-center">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-          consequuntur voluptates itaque, perspiciatis pariatur unde veniam
-          culpa obcaecati soluta nesciunt autem, incidunt quia rem labore cum
-          magni eaque. Cupiditate, blanditiis.
-        </p>
-        <div className="ml-2">
-          <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-            {stats.map((item) => (
-              <div
-                key={item.id}
-                className="relative bg-white pt-5 px-4 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden"
-              >
-                <dt>
-                  <div className="absolute bg-primaryBlue rounded-md p-3">
-                    <item.icon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  <p className="ml-16 text-sm font-medium text-gray-500 truncate">
-                    {item.name}
-                  </p>
-                </dt>
-                <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
-                  <p className="text-2xl font-semibold text-gray-900">
-                    {item.stat}
-                  </p>
-                </dd>
-              </div>
-            ))}
-          </dl>
-        </div>
-        <div className="m-4 mt-6">
-          <div className="md:flex md:justify-between items-center mb-2">
-            <div className="font-semibold text-xl">Leave a Review:</div>
-            <div className="md:w-[18vw] flex md:justify-end mt-4 md:mt-0 space-x-2">
-              <div>Rating</div>
-              <Rating onChange={ratingChangeHandler} />
-            </div>
+      {!loading && (
+        <div className="flex-col items-center justify-center">
+          <p className=" flex items-center justify-center  font-semibold text-gray-500">
+            INSTRUCTOR
+          </p>
+          <p className=" flex items-center justify-center font-bold text-4xl">
+            {receivedData.instructor.firstName}{" "}
+            {receivedData.instructor.lastName}
+          </p>
+          <p className="flex items-center justify-center text-gray-500">
+            About Me
+          </p>
+          <p className="flex items-center justify-center text-center">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            consequuntur voluptates itaque, perspiciatis pariatur unde veniam
+            culpa obcaecati soluta nesciunt autem, incidunt quia rem labore cum
+            magni eaque. Cupiditate, blanditiis.
+          </p>
+          <div className="ml-2">
+            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+              {stats.map((item) => (
+                <div
+                  key={item.id}
+                  className="relative bg-white pt-5 px-4 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden"
+                >
+                  <dt>
+                    <div className="absolute bg-primaryBlue rounded-md p-3">
+                      <item.icon
+                        className="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <p className="ml-16 text-sm font-medium text-gray-500 truncate">
+                      {item.name}
+                    </p>
+                  </dt>
+                  <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
+                    <p className="text-2xl font-semibold text-gray-900">
+                      {item.stat}
+                    </p>
+                  </dd>
+                </div>
+              ))}
+            </dl>
           </div>
-          <textarea
-            reviewState={enteredReview}
-            onChange={reviewChangeHandler}
-            className="w-full bg-white border border-slate-300 rounded-md text-sm shadow-sm
+          <div className="m-4 mt-6">
+            <div className="md:flex md:justify-between items-center mb-2">
+              <div className="font-semibold text-xl">Leave a Review:</div>
+              <div className="md:w-[18vw] flex md:justify-end mt-4 md:mt-0 space-x-2">
+                <div>Rating</div>
+                <Rating onChange={ratingChangeHandler} />
+              </div>
+            </div>
+            <textarea
+              reviewState={enteredReview}
+              onChange={reviewChangeHandler}
+              className="w-full bg-white border border-slate-300 rounded-md text-sm shadow-sm
             focus:outline-none focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue"
-          />
-          <div className="flex justify-end mb-4">
-            <SecondaryButton text="Submit" onClick={onClickHandler} />
+            />
+            <div className="flex justify-end mb-4">
+              <SecondaryButton text="Submit" onClick={onClickHandler} />
+            </div>
+            <div>
+              <div className=" font-semibold text-xl mb-4">
+                Instructor Reviews:
+              </div>
+              <div className=" mb-6 mx-12">
+                <AverageSummary count={receivedData.count} />
+              </div>
+              <div className=" mx-8">{displayedReviews}</div>
+            </div>
           </div>
           <div>
             <div className=" font-semibold text-xl mb-4">
-              Instructor Reviews:
+              Instructor Courses:
             </div>
-            <div className=" mb-6 mx-12">
-              <AverageSummary
-                count={[
-                  { rating: 1, count: 12 },
-                  { rating: 2, count: 1 },
-                  { rating: 3, count: 22 },
-                  { rating: 4, count: 25 },
-                  { rating: 5, count: 7 },
-                ]}
-              />
-            </div>
-            <div className=" mx-8">{displayedReviews}</div>
+            <div>{displayedCourses}</div>
           </div>
         </div>
-        {/* <div className="flex justify-center items-center mt-4">
-          {receivedData.myCourses.length !== 0 && (
-            <ThemeProvider theme={theme}>
-              <Pagination
-                className="ml-2 mr-2"
-                count={Math.ceil(noOfPages / 10)}
-                color="primary"
-                page={page}
-                onChange={onChangePageHandler}
-              />
-            </ThemeProvider>
-          )}
-        </div> */}
-      </div>
+      )}
     </Fragment>
   );
 };

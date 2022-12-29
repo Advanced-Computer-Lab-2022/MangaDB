f5f9965c42e21c25dbd4e190092b3ad1c20cea68
mina0magdy <92601097+mina0magdy@users.noreply.github.com>
2022-12-29 22:15:13 +0200

    Added validation and feedback

--- frontend/src/components/AddSubtitles/SourceForm.jsx
+++ frontend/src/components/AddSubtitles/SourceForm.jsx
@@ -13,6 +13,10 @@ const SourceForm = (props) => {
   const [sourceType, setSourceType] = useState("Video");
   const [sourceDuration, setSourceDuration] = useState(0);
   const containerRef = useRef(null);
+  const [emptySourseTitle, setEmptySourseTitle] = useState(false);
+  const [emptySourseLink, setEmptySourseLink] = useState(false);
+  const [validSourceLink, setValidSourceLink] = useState(true);
+  const [warningMessage, setWarningMessage] = useState("");
 
   const [examState, setExamState] = useState([]);
 
@@ -59,6 +63,32 @@ const SourceForm = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+    var currentWarningMessage = "";
+    if (!validateYouTubeUrl(sourceLink) && sourceType === "Video") {
+      setValidSourceLink(false);
+      currentWarningMessage = "Please enter a valid youtube link ";
+    } else {
+      setValidSourceLink(true);
+    }
+    if (titleRef.current.value === "") {
+      setEmptySourseTitle(true);
+      currentWarningMessage = "Please fill the following fields ";
+    } else {
+      setEmptySourseTitle(false);
+    }
+    if (sourceLink === "" && sourceType === "Video") {
+      setEmptySourseLink(true);
+      currentWarningMessage = "Please fill the following fields ";
+    } else {
+      setEmptySourseLink(false);
+    }
+    
+    if (currentWarningMessage !== "") {
+      setWarningMessage(currentWarningMessage);
+      return;
+    }
+
+
     const title = titleRef.current.value;
     const link = sourceLink;
     var sourceData;
@@ -134,14 +164,13 @@ const SourceForm = (props) => {
     displayedForm = (
       <div className="third-control">
         <label className="block" htmlFor="link">
-          Link
+          Link <span className="text-red-500">*</span>
         </label>
         <input
           value={sourceLink}
           onChange={linkChangeHandler}
           id="link"
-          className="w-full mt-1 px-3 py-1 bg-white border border-slate-300 rounded-md text-sm shadow-sm
-focus:outline-none focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue"
+          className={"w-full mt-1 px-3 py-1 bg-white border border-slate-300 rounded-md text-sm shadow-sm focus:outline-none focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue ".concat(emptySourseTitle || !validSourceLink ? "border-red-500" : "")}
         ></input>
       </div>
     );
@@ -163,17 +192,53 @@ focus:outline-none focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue"
           </button>
         </div>
       </div>
+
+      <div>
+        <h2 className="text-lg leading-6 font-medium text-gray-900 mb-1">
+          Personal Information
+        </h2>
+        <p className="mt-1 text-sm text-gray-500">
+          This information will be displayed publicly so be careful what you
+          share.
+        </p>
+      </div>
+      <div
+        class={
+          emptySourseLink || emptySourseTitle || !validSourceLink
+            ? "px-4 py-3 mt-3 text-red-900 bg-red-50 border rounded-md"
+            : "hidden"
+        }
+      >
+        <div class="flex justify-between flex-wrap">
+          <div class="w-0 flex-1 flex">
+            <div class="mr-3 pt-1">
+              <svg
+                width="26"
+                height="26"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+              >
+                <path d="M13.6086 3.247l8.1916 15.8c.0999.2.1998.5.1998.8 0 1-.7992 1.8-1.7982 1.8H3.7188c-.2997 0-.4995-.1-.7992-.2-.7992-.5-1.1988-1.5-.6993-2.4 5.3067-10.1184 8.0706-15.385 8.2915-15.8.3314-.6222.8681-.8886 1.4817-.897.6135-.008 1.273.2807 1.6151.897zM12 18.95c.718 0 1.3-.582 1.3-1.3 0-.718-.582-1.3-1.3-1.3-.718 0-1.3.582-1.3 1.3 0 .718.582 1.3 1.3 1.3zm-.8895-10.203v5.4c0 .5.4.9.9.9s.9-.4.9-.9v-5.3c0-.5-.4-.9-.9-.9s-.9.4-.9.8z"></path>
+              </svg>
+            </div>
+            <div>
+              <h4 class="text-md mt-[5px] leading-6 font-medium">{warningMessage}</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <form onSubmit={submitHandler}>
         <div className="space-y-4">
           <div className="first control">
             <label className="block" htmlFor="source-title">
-              Source Title
+              Source Title <span className="text-red-500">*</span>
             </label>
             <input
               ref={titleRef}
               id="source-title"
-              className="w-full mt-1 px-3 py-1 bg-white border border-slate-300 rounded-md text-sm shadow-sm
-            focus:outline-none focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue"
+              className={"w-full mt-1 px-3 py-1 bg-white border border-slate-300 rounded-md text-sm shadow-sm focus:outline-none focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue ".concat(emptySourseTitle ? "border-red-500" : "")}
             />
           </div>
           <div className="flex gap-2">
--- frontend/src/pages/CourseDetailsPage.jsx
+++ frontend/src/pages/CourseDetailsPage.jsx
@@ -7,6 +7,7 @@ import AddToCartCard from "../components/CourseDetailsComp/AddToCartCard";
 import CourseContent from "../components/CourseDetailsComp/CourseContent";
 import CourseReviews from "../components/CourseDetailsComp/CourseReviews";
 import { useLocation } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
 const CourseDetailsPage = () => {
   const location = useLocation();
@@ -15,28 +16,30 @@ const CourseDetailsPage = () => {
   const [userRegistered, setUserRegistered] = useState(false);
   const [courseReviews, setCourseReviews] = useState([]);
   const [reviewsCount, setReviewsCount] = useState([]);
+  const [corp, setCorp] = useState(false);
+  const [requested, setRequested] = useState(false);
   useEffect(() => {
     const courseId = location.state.courseId;
     axios
       .get(
         "http://localhost:3000/course/"
           .concat(courseId)
-          .concat("?uid=63a37e9688311fa832f43336")
+          .concat("?uid=638a07cdbc3508481a2d7da9")
       )
       .then((res) => {
         setCourseDetails(res.data.course);
-        console.log(res.data.course);
+        console.log(res.data);
         setLoaded(true);
-        if (
-          res.data.userData!==null
-        
-              ) {
-                setUserRegistered(true);
-              } else {
-                setUserRegistered(false);
-              }
+        if (res.data.userData !== null) {
+          if (res.data.userData.role === "corporate") {
+            //check if requested before
+            setCorp(true);
+          } else setUserRegistered(true);
+        } else {
+          setUserRegistered(false);
+        }
       });
-    const userId = "63a37e9688311fa832f43336";
+    const userId = "638a07cdbc3508481a2d7da9";
    
     // axios
     //   .post(`http://localhost:3000/invoice/${location.state.courseId}`, {
@@ -57,9 +60,7 @@ const CourseDetailsPage = () => {
       // setCourseDetails(res.data.course);
       // console.log(res.data.course);
       // setLoaded(true);
-      setCourseReviews(res.data.review);
-      console.log(res.data.count);
-      setReviewsCount(res.data.count);
+      console.log(res.data.review);
     });
 
     // axios
@@ -83,57 +84,61 @@ const CourseDetailsPage = () => {
   };
 
   return (
-    <Fragment>
-      <NavBar />
-      <div className="bg-veryLightBlue py-4 px-6 flex justify-between">
-        <CourseDetailsCard
-          courseTitle={courseDetails.courseTitle}
-          level={courseDetails.level}
-          instructorName={courseDetails.instructorName}
-          subject={courseDetails.subject}
-          courseDescription={courseDetails.courseDescription}
-          rating={courseDetails.rating}
-          discount={courseDetails.discount}
-          coursePrice={courseDetails.coursePrice}
-          discountedPrice={courseDetails.discountedPrice}
-          currencySymbol="$"
-        />
-      </div>
-      <AddToCartCard
-        courseOverview={courseDetails.courseOverview}
-        id={courseDetails._id}
-        userRegister={userRegistered}
-        courseImage={courseDetails.courseImage}
-      />
-      <div className="text-xl font-semibold py-4 mx-10 md:w-7/12">
-        <div className="mb-3">Course Summary</div>
-        <div
-          className="ml-10 align-middle font-normal"
-          dangerouslySetInnerHTML={{ __html: courseDetails.summary }}
-        ></div>
-      </div>
-      {loaded && (
-        <CourseContent
-          content={courseDetails.subtitles}
-          courseDuration={courseDetails.totalMins}
-        />
-      )}
-      <div className="text-xl font-semibold py-4 mx-10 md:w-7/12">
-        <div className="mb-3">Course Requirements</div>
-        <div
-          className="ml-10 font-normal"
-          dangerouslySetInnerHTML={{ __html: courseDetails.requirements }}
-        ></div>
-      </div>
-      {loaded && (
-        <CourseReviews
-          reviews={courseReviews}
-          reviewsCount={reviewsCount}
-          onSubmit={submitReviewHandler}
+    <SnackbarProvider maxSnack={3}>
+      <Fragment>
+        <NavBar />
+        <div className="bg-veryLightBlue py-4 px-6 flex justify-between">
+          <CourseDetailsCard
+            courseTitle={courseDetails.courseTitle}
+            level={courseDetails.level}
+            instructorName={courseDetails.instructorName}
+            subject={courseDetails.subject}
+            courseDescription={courseDetails.courseDescription}
+            rating={courseDetails.rating}
+            discount={courseDetails.discount}
+            coursePrice={courseDetails.coursePrice}
+            discountedPrice={courseDetails.discountedPrice}
+            currencySymbol="$"
+          />
+        </div>
+        <AddToCartCard
+          courseOverview={courseDetails.courseOverview}
+          id={courseDetails._id}
           userRegister={userRegistered}
+          courseImage={courseDetails.courseImage}
+          requested={requested}
+          corp={corp}
         />
-      )}
-    </Fragment>
+        <div className="text-xl font-semibold py-4 mx-10 md:w-7/12">
+          <div className="mb-3">Course Summary</div>
+          <div
+            className="ml-10 align-middle font-normal"
+            dangerouslySetInnerHTML={{ __html: courseDetails.summary }}
+          ></div>
+        </div>
+        {loaded && (
+          <CourseContent
+            content={courseDetails.subtitles}
+            courseDuration={courseDetails.totalMins}
+          />
+        )}
+        <div className="text-xl font-semibold py-4 mx-10 md:w-7/12">
+          <div className="mb-3">Course Requirements</div>
+          <div
+            className="ml-10 font-normal"
+            dangerouslySetInnerHTML={{ __html: courseDetails.requirements }}
+          ></div>
+        </div>
+        {loaded && (
+          <CourseReviews
+            reviews={courseReviews}
+            reviewsCount={reviewsCount}
+            onSubmit={submitReviewHandler}
+            userRegister={userRegistered}
+          />
+        )}
+      </Fragment>
+    </SnackbarProvider>
   );
 };
 
